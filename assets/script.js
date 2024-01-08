cd91ab4d5d56fad5e6117ce5d760191e0785ee59
shannontice <stice14@gmail.com>
2024-01-07 20:51:38 -0500

    blue

--- assets/script.js
+++ assets/script.js
@@ -141,7 +141,7 @@ function getWeather(coordinates, cityName) {
 
     // make a div that will hold the weather card
     // it's time to d-d-d-d-duel
-    var card = $(`<div class="weather-card space-y-2 border-double border-8 border-sky-500 rounded-lg text-center bg-white ${cityName.toLowerCase()}">`)
+    var card = $(`<div class="weather-card space-y-2 border-double border-8 border-cyan-900 rounded-lg text-center bg-cyan-700 text-white ${cityName.toLowerCase()}">`)
     // make some sections in the card for waves data and weather
     card.append(`<h3 class="title text-3xl"></h3>
                 <div class="surf-content"></div>
